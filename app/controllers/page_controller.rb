57ac5ba78c5e5c78fa8b8fad3aad188c8b681ea6
theachyutkadam <theachyutkadam@gmail.com>
2019-08-21 22:07:24 +0530

    change file extention and add thair methods with routes

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,4 +1,16 @@
 class PageController < ApplicationController
   def index
   end
+  def about
+  end
+  def cars
+  end
+  def contact
+  end
+  def element
+  end
+  def service
+  end
+  def team
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # get 'page/index'
   root to: 'page#index'
+  # resources :pages
+  get 'page', :to => 'page#contact'
+  get 'page', :to => 'page#about'
+  get 'page', :to => 'page#cars'
+  get 'page', :to => 'page#contact'
+  get 'page', :to => 'page#elements'
+  get 'page', :to => 'page#service'
+  get 'page', :to => 'page#team'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
