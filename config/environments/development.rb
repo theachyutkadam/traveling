e6dc9eeb6c4e72d3de1568cc66f9440e9c5aebfe
theachyutkadam <theachyutkadam@gmail.com>
2019-08-31 16:22:11 +0530

    send username and pass when turn on the server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,8 +37,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'smtp.gmail.com',
-    user_name: 'user_name',
-    password: 'password',
+    user_name: RAILS_ENV["USERNAME"],
+    password: RAILS_ENV['PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true
   }
