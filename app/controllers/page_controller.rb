6273e901c40eeadf7a5f6a27e4d2fdbc6b76a6dd
theachyutkadam <theachyutkadam@gmail.com>
2019-08-31 16:20:30 +0530

    remove unusable methods

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,17 +2,9 @@ class PageController < ApplicationController
   def index
 
   end
-  def about
-  end
-  def cars
-  end
+
   def contact
     UserMailer.with(user: params.permit![:page]).welcome_email(params.permit![:page]).deliver_later
   end
-  def element
-  end
-  def service
-  end
-  def team
-  end
+
 end
