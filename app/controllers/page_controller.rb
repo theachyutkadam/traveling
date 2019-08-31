73d20185651e93c4461c5bf92298c7f3edd29396
theachyutkadam <theachyutkadam@gmail.com>
2019-08-31 17:39:43 +0530

    redirect page after send mail

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,10 +1,9 @@
 class PageController < ApplicationController
   def index
-
   end
-
   def contact
     UserMailer.with(user: params.permit![:page]).welcome_email(params.permit![:page]).deliver_later
+    redirect_to root_path
   end
 
 end
